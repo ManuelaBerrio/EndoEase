addc17ebf19bfe5068864f9c99d4ae76e8767de2
Manuela Berrio <mbcma2604@gmail.com>
2023-06-27 13:21:12 -0500

    Initial Commit

---
+++ EndoEase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EndoEase
+//
+//  Created by Jairo ivan Berrio munoz on 27/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EndoEase/EndoEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndoEaseApp.swift
+//  EndoEase
+//
+//  Created by Jairo ivan Berrio munoz on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EndoEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
