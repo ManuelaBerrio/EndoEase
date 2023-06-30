ec9dacb91a5c52c9be919314111a49863efc4019
Saranya Mandapaty <saranya.broadway@gmail.com>
2023-06-29 18:54:44 -0700

    established final skeleton

--- SelectMySymptomsView.swift
+++ SelectMySymptomsView.swift
@@ -8,138 +8,135 @@
 import Foundation
 import SwiftUI
 struct SelectMySymptomsView: View {
-let symptomToEmoji = [
-"Vomiting": "🤢",
-"Fainting spells": "🥴",
-"Dizziness": "🥺",
-"Headaches": "🤕",
-"Migraines": "😖",
-"Body chills": "🥶",
-"Heavy bleeding": "🩸"]
-
-@State private var vomitingSelected = false
-@State private var faintingSpellsSelected = false
-@State private var dizzinessSelected = false
-@State private var headachesSelected = false
-@State private var migrainesSelected = false
-@State private var bodyChillsSelected = false
-@State private var heavyBleedingSelected = false
-
-var body: some View {
-    NavigationView {
-        VStack(spacing: 20) {
-            Spacer()
-            
-            ZStack {
-                Image("Daisy")
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
+    let symptomToEmoji = [
+        "Vomiting": "🤢",
+        "Fainting spells": "🥴",
+        "Dizziness": "🥺",
+        "Headaches": "🤕",
+        "Migraines": "😖",
+        "Body chills": "🥶",
+        "Heavy bleeding": "🩸"
+    ]
+    
+    @State private var vomitingSelected = false
+    @State private var faintingSpellsSelected = false
+    @State private var dizzinessSelected = false
+    @State private var headachesSelected = false
+    @State private var migrainesSelected = false
+    @State private var bodyChillsSelected = false
+    @State private var heavyBleedingSelected = false
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Spacer()
                 
-                VStack(spacing: 20) {
-                    Spacer()
+                ZStack {
+                    Image("Home")
+                        .resizable()
+                        .ignoresSafeArea()
                     
                     VStack(spacing: 20) {
-                        Button(action: {
-                            vomitingSelected.toggle()
-                        }) {
-                            Text("Vomiting")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
-                        }
-                        .background(vomitingSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
+                        Spacer()
                         
-                        Button(action: {
-                            faintingSpellsSelected.toggle()
-                        }) {
-                            Text("Fainting Spells")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
+                        VStack(spacing: 20) {
+                            Button(action: {
+                                vomitingSelected.toggle()
+                            }) {
+                                Text("Vomiting")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(vomitingSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                faintingSpellsSelected.toggle()
+                            }) {
+                                Text("Fainting Spells")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(faintingSpellsSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                dizzinessSelected.toggle()
+                            }) {
+                                Text("Dizziness")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(dizzinessSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                headachesSelected.toggle()
+                            }) {
+                                Text("Headaches")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(headachesSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                migrainesSelected.toggle()
+                            }) {
+                                Text("Migraines")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(migrainesSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                bodyChillsSelected.toggle()
+                            }) {
+                                Text("Body Chills")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(bodyChillsSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
+                            
+                            Button(action: {
+                                heavyBleedingSelected.toggle()
+                            }) {
+                                Text("Heavy Bleeding")
+                                    .font(.title3)
+                                    .foregroundColor(.black)
+                                    .padding([.top, .bottom], 10)
+                                    .padding([.leading, .trailing], 15)
+                            }
+                            .background(heavyBleedingSelected ? Color.yellow : Color.gray)
+                            .cornerRadius(10)
                         }
-                        .background(faintingSpellsSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
                         
-                        Button(action: {
-                            dizzinessSelected.toggle()
-                        }) {
-                            Text("Dizziness")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
+                        NavigationLink(destination: LearnMoreView()) {
+                            Text("Next")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 30)
+                                .background( Color.yellow)
+                                .cornerRadius(10)
                         }
-                        .background(dizzinessSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
-                        
-                        Button(action: {
-                            headachesSelected.toggle()
-                        }) {
-                            Text("Headaches")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
-                        }
-                        .background(headachesSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
-                        
-                        Button(action: {
-                            migrainesSelected.toggle()
-                        }) {
-                            Text("Migraines")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
-                        }
-                        .background(migrainesSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
-                        
-                        Button(action: {
-                            bodyChillsSelected.toggle()
-                        }) {
-                            Text("Body Chills")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
-                        }
-                        .background(bodyChillsSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
-                        
-                        Button(action: {
-                            heavyBleedingSelected.toggle()
-                        }) {
-                            Text("Heavy Bleeding")
-                                .font(.title3)
-                                .foregroundColor(.black)
-                                .padding([.top, .bottom], 10)
-                                .padding([.leading, .trailing], 15)
-                        }
-                        .background(heavyBleedingSelected ? Color.yellow : Color.gray)
-                        .cornerRadius(10)
                     }
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: LearnMoreView()) {
-                        Text("Next")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity, minHeight: 50)
-                            .background(Color.yellow)
-                            .cornerRadius(10.0)
-                            .padding(20.0)
-                    }
-                }
                 }
             }
         }
     }
-}
 
+    }
